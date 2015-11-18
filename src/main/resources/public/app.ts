0c606f0024f2587c89acc4f50543574724e9d635
como-quesito <rapidclimate@outlook.com>
2015-11-18 00:23:58 -0600

    enojadisimo

--- src/main/resources/public/app.ts
+++
@@ -1,8 +0,0 @@
-import {bootstrap, Component} from 'angular2/angular2';
-@Component({
-    selector: 'my-app',
-    template: '<h1>Mi primer app angulario</h1>'
-})
-class AppComponent { 
-    }
-bootstrap(AppComponent);
